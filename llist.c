caae786275581173a9af4c43d0a5080f6a7c2c18
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-09 14:23:01 -0800

    node free/alloc + basic link logic

--- llist.c
+++ llist.c
@@ -4,57 +4,72 @@
 #include<stdlib.h>
 
 void llist_insert_head(struct node **head, struct node *n){
-
+    *head=n;
 }
 
 struct node *llist_delete_head(struct node **head){
-    return NULL;
+    struct node *n=*head;
+    head=NULL;
+    return n;
 }
 
 void llist_insert_tail(struct node **head, struct node *n){
-
+    *head=n;
 }
 
 void llist_print(struct node *head){
     if(head==NULL){
         printf("[empty]\n");
     }else{
-        
+        printf("%d", head->value);
     }
 }
 
 void llist_free(struct node **head){
-
+    node_free(*head);
 }
 
 struct node *node_alloc(int value){
-    return NULL;
+    struct node *n;
+    n=malloc(16);//sizeof(struct node is 16)
+    n->value=value;
+    n->next=NULL;
+    return n;
 }
 
 void node_free(struct node *n){
-
+    free(n);
 }
-
-
 
 int main(int argc, char *argv[]){
     struct node *head=NULL;
+    
     if(argc>1){
         int i=1;
         while(argv[i]!=NULL){
             if(strcmp(argv[i],"ih")==0){
-                printf("insertHead %d, ", atoi(argv[i+1]));
+                //printf("insertHead %d, ", atoi(argv[i+1]));
+                struct node *n=node_alloc(atoi(argv[i+1]));
+                llist_insert_head(&head,n);
                 i+=2;
-            }else if(strcmp(argv[i],"it")==0){
-                printf("insertTail %d, ", atoi(argv[i+1]));
+            }
+            else if(strcmp(argv[i],"it")==0){
+                //printf("insertTail %d, ", atoi(argv[i+1]));
+                struct node *n=node_alloc(atoi(argv[i+1]));
+                llist_insert_tail(&head, n);
                 i+=2;
-            }else if(strcmp(argv[i],"dh")==0){
-                printf("deleteHead, ");
+            }
+            else if(strcmp(argv[i],"dh")==0){
+                //printf("deleteHead, ");
+                llist_delete_head(&head);
                 i++;
-            }else if(strcmp(argv[i],"f")==0){
-                printf("free, ");
+            }
+            else if(strcmp(argv[i],"f")==0){
+                //printf("free, ");
+                llist_free(&head);
                 i++;
-            }else if(strcmp(argv[i],"p, ")==0){
+            }
+            else if(strcmp(argv[i],"p")==0){
                 llist_print(head);
                 i++;
             }
@@ -64,5 +79,7 @@ int main(int argc, char *argv[]){
             }
         }
     }
-    llist_free(&head);
+    if(head!=NULL){
+        llist_free(&head);
+    }
 }
