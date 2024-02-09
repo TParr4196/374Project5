b3e04d0212378952c96b8090f0dd4e39f660e8cb
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-09 13:51:12 -0800

    command line parsing

--- llist.c
+++ llist.c
@@ -1,5 +1,7 @@
 #include "llist.h"
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 void llist_insert_head(struct node **head, struct node *n){
 
@@ -14,13 +16,53 @@ void llist_insert_tail(struct node **head, struct node *n){
 }
 
 void llist_print(struct node *head){
-    printf("[empty]\n");
+    if(head==NULL){
+        printf("[empty]\n");
+    }else{
+        
+    }
 }
 
 void llist_free(struct node **head){
 
 }
 
+struct node *node_alloc(int value){
+    return NULL;
+}
+
+void node_free(struct node *n){
+
+}
+
+
+
 int main(int argc, char *argv[]){
-    llist_print(NULL);
+    struct node *head=NULL;
+    if(argc>1){
+        int i=1;
+        while(argv[i]!=NULL){
+            if(strcmp(argv[i],"ih")==0){
+                printf("insertHead %d, ", atoi(argv[i+1]));
+                i+=2;
+            }else if(strcmp(argv[i],"it")==0){
+                printf("insertTail %d, ", atoi(argv[i+1]));
+                i+=2;
+            }else if(strcmp(argv[i],"dh")==0){
+                printf("deleteHead, ");
+                i++;
+            }else if(strcmp(argv[i],"f")==0){
+                printf("free, ");
+                i++;
+            }else if(strcmp(argv[i],"p, ")==0){
+                llist_print(head);
+                i++;
+            }
+            else{
+                printf("command %d unknown, ", i-1);
+                i++;
+            }
+        }
+    }
+    llist_free(&head);
 }
