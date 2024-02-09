d3a0c21498b1e97760e1b06a0ef4f3a02176513c
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-09 13:13:45 -0800

    first commit

---
+++ llist.c
@@ -0,0 +1,26 @@
+#include "llist.h"
+#include<stdio.h>
+
+void llist_insert_head(struct node **head, struct node *n){
+
+}
+
+struct node *llist_delete_head(struct node **head){
+    return NULL;
+}
+
+void llist_insert_tail(struct node **head, struct node *n){
+
+}
+
+void llist_print(struct node *head){
+    printf("[empty]\n");
+}
+
+void llist_free(struct node **head){
+
+}
+
+int main(int argc, char *argv[]){
+    llist_print(NULL);
+}
